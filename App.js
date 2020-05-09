4df136007c7657262878f8f738f7cc9f5969c951
Sanjay Singh Bisht <sanjaysinghbisht114@gmail.com>
2020-05-09 20:50:43 +0530

    Done with input fields

--- App.js
+++ App.js
@@ -1,19 +1,66 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{Component} from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+const cpr={
+  DOLLAR:0.014,
+  EURO:0.012,
+  POUND:0.011,
+  RUBEL:0.93,
+  AUSDOLLAR:0.2,
+  CANDOLLAR:0.019,
+  YEN:1.54,
+  DINAR:0.0043,
+  BITCOIN:0.0000041,
+};
+
+export default class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      inp:'',
+      res:'0.00'
+    };
+  }
+  render(){
+    return(
+      <View style={styles.container}>
+        <View style={styles.screenv}>
+        <View style={styles.resc}>
+          <Text style={styles.resv}>
+            {this.state.res}
+          </Text>
+        </View>
+        <View style={styles.ic}>
+          <TextInput style={styles.ip} selectionColor='#fff' keyboardType='numeric'
+            placeholder='Enter Value ...' onChangeText={inp=>this.setState({inp})}/>
+        </View>
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'white',
+    marginTop:30
   },
+  screenv:{
+    flex:1
+  },
+  resc:{
+    height:70,
+    marginTop:10,
+    justifyContent:'center',
+    borderColor:'#c1c1c1',
+    backgroundColor:'#0A79DE',
+    alignItems:'center',
+    borderWidth:2
+  },
+  resv:{
+    fontSize:30,
+    fontWeight:'bold',
+    color:'white'
+  }
 });
